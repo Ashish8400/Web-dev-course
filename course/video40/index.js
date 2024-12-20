1c050ae346e933bf2cd331fc4fff2288482f92eb
Ashish Yadav <128246175+Ashish8400@users.noreply.github.com>
2024-12-20 22:23:38 +0530

    new

---
+++ course/video40/index.js
@@ -0,0 +1,26 @@
+// var age= 25;
+// if(true){
+//     console.log(age);
+// }
+
+// function solve(){
+//     var age = 25;
+//     console.log(age);
+// }
+// console.log(age);
+//  solve();
+
+  // 
+   
+  let i = 1;
+  while(i<=6){
+    if(i==3){
+      i++;
+      continue;
+    }
+    else{
+      console.log(i);
+      i++;
+
+    }
+  }
